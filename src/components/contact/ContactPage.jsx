fb5c604bf170364fa115b52c98e36c14a98cb955
Yongje Kim <yongjekim1009@gmail.com>
2025-09-14 22:18:54 +0900

    연결 페이지 작업

--- src/components/contact/ContactPage.jsx
+++ src/components/contact/ContactPage.jsx
@@ -1,16 +1,37 @@
 import styles from "./ContactPage.module.css";
 
+import phone from "../../assets/images/contact/phone-icon.svg";
+import email from "../../assets/images/contact/email-icon.svg";
+
 const ContactPage = () => {
   return (
-    <div>
-      <div className={styles["contact-container"]}>
-        <div className={styles["contact-wrapper"]}>
-          <h1>Cantact</h1>
-          <p>
-            작은 아이디어라도 함께 나누며
-            <br />
-            새로운 기회를 만들어가고 싶습니다
-          </p>
+    <div className={styles["contact-container"]}>
+      <div className={styles["contact-wrapper"]}>
+        <h1>Cantact</h1>
+        <p>
+          작은 아이디어라도 함께 나누며
+          <br />
+          새로운 기회를 만들어가고 싶습니다
+        </p>
+      </div>
+      <div className={styles["contact-group"]}>
+        <div className={styles["contact-section"]}>
+          <div className={styles["contact-icon"]}>
+            <img src={phone} alt="전화 아이콘" />
+          </div>
+          <div className={styles["contact-type"]}>Phone</div>
+          <div className={styles["contact-link"]}>010 . 9911 . 2518</div>
+        </div>
+
+        <div className={styles["contact-section"]}>
+          <div className={styles["contact-icon"]}>
+            <img src={email} alt="이메일 아이콘" />
+          </div>
+          <div className={styles["contact-type"]}>Email</div>
+          <div className={styles["contact-link"]}>
+            dydwp1009
+            <div className={styles.address}>@naver.com</div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/portfolio/PortfolioListPage.jsx
+++ src/components/portfolio/PortfolioListPage.jsx
@@ -7,22 +7,20 @@ const PortfolioListPage = () => {
   const [selectedCategory, setSelectedCategory] = useState("promotion");
 
   return (
-    <div>
-      <div className={styles["portolio-container"]}>
-        <div className={styles["portolio-wrapper"]}>
-          <h1>Portfolio</h1>
-          <p>
-            사용자의 시선을 사로잡고
-            <br />
-            목적을 이끄는 디자인을 만들어갑니다
-          </p>
-        </div>
-        <CategoryButton
-          selected={selectedCategory}
-          onSelect={setSelectedCategory}
-        />
-        <PortfolioThumbnail category={selectedCategory} />
+    <div className={styles["portolio-container"]}>
+      <div className={styles["portolio-wrapper"]}>
+        <h1>Portfolio</h1>
+        <p>
+          사용자의 시선을 사로잡고
+          <br />
+          목적을 이끄는 디자인을 만들어갑니다
+        </p>
       </div>
+      <CategoryButton
+        selected={selectedCategory}
+        onSelect={setSelectedCategory}
+      />
+      <PortfolioThumbnail category={selectedCategory} />
     </div>
   );
 };
